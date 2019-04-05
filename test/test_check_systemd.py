a1feeb488aa8db1d350893c565a98dcf97d27136
Josef Friedrich <josef@friedrich.rocks>
2019-04-05 16:27:31 +0200

    Mock script

--- test/test_check_systemd.py
+++ test/test_check_systemd.py
@@ -1,6 +1,11 @@
 import unittest
 import os
 import subprocess
+from unittest import mock
+import check_systemd
+from jfscripts._utils import Capturing
+import io
+from contextlib import redirect_stdout
 
 BIN = os.path.abspath(os.path.join(os.path.dirname(__file__), 'bin'))
 os.environ['PATH'] = BIN + ':' + os.environ['PATH']
@@ -28,16 +33,17 @@ class TestCheckSystemd(unittest.TestCase):
         self.assertEqual(process.stdout, 'SYSTEMD OK - all\n')
 
     def test_failure(self):
-        process = subprocess.run(
-            ['./check_systemd.py'],
-            encoding='utf-8',
-            stdout=subprocess.PIPE,
-        )
-        self.assertEqual(process.returncode, 2)
-        self.assertEqual(
-            process.stdout,
-            'SYSTEMD CRITICAL - test.service: failed\n'
-        )
+        with mock.patch('sys.exit') as sys_exit, \
+             mock.patch('check_systemd.subprocess.Popen') as Popen, \
+             mock.patch('sys.argv', ['check_systemd.py']):
+            process = Popen.return_value
+            process.communicate.return_value = (b'', None)
+            f = io.StringIO()
+            with redirect_stdout(f):
+                check_systemd.main()
+
+        sys_exit.assert_called_with(0)
+        self.assertEqual(f.getvalue(), '')
 
     def test_exclusive_group(self):
         process = subprocess.run(
