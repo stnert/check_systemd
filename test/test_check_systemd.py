2373a6858b701fee8af26e77ac1e6d272fe43343
Josef Friedrich <josef@friedrich.rocks>
2019-04-05 08:48:38 +0200

    Add more tests

--- test/test_check_systemd.py
+++ test/test_check_systemd.py
@@ -20,6 +20,41 @@ class TestCheckSystemd(unittest.TestCase):
             'SYSTEMD CRITICAL - test.service: failed\n'
         )
 
+    def test_exclusive_group(self):
+        process = subprocess.run(
+            ['./check_systemd.py', '-s', 'test1.service', '-e',
+             'test2.service'],
+            encoding='utf-8',
+            stderr=subprocess.PIPE
+        )
+        self.assertEqual(process.returncode, 2)
+        self.assertIn(
+            'error: argument -e/--exclude: not allowed with argument '
+            '-s/--service',
+            process.stderr,
+        )
+
+    def test_option_exclude_known_service(self):
+        process = subprocess.run(
+            ['./check_systemd.py', '-e', 'test.service'],
+            encoding='utf-8',
+            stdout=subprocess.PIPE
+        )
+        self.assertEqual(process.returncode, 0)
+        self.assertEqual(process.stdout, 'SYSTEMD OK - all\n')
+
+    def test_option_exclude_unknown_service(self):
+        process = subprocess.run(
+            ['./check_systemd.py', '-e', 'testX.service'],
+            encoding='utf-8',
+            stdout=subprocess.PIPE
+        )
+        self.assertEqual(process.returncode, 2)
+        self.assertEqual(
+            process.stdout,
+            'SYSTEMD CRITICAL - test.service: failed\n'
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
