9f85911270ee1c3c36e4a9de9e8d726b36338163
Josef Friedrich <josef@friedrich.rocks>
2019-04-06 14:32:05 +0200

    Add more tests

--- test/test_subprocess.py
+++ test/test_subprocess.py
@@ -33,6 +33,16 @@ class TestSubprocess(unittest.TestCase):
         self.assertEqual(process.returncode, 0)
         self.assertEqual(process.stdout, 'SYSTEMD OK - all\n')
 
+    def test_ok_verbose(self):
+        with AddBin('bin/ok'):
+            process = subprocess.run(
+                ['./check_systemd.py', '--verbose'],
+                encoding='utf-8',
+                stdout=subprocess.PIPE,
+            )
+        self.assertEqual(process.returncode, 0)
+        self.assertEqual(process.stdout, 'SYSTEMD OK - all\nok: all\n')
+
     def test_failure(self):
         with AddBin('bin/failure'):
             process = subprocess.run(
@@ -43,6 +53,52 @@ class TestSubprocess(unittest.TestCase):
         self.assertEqual(process.returncode, 2)
         self.assertEqual(process.stdout,
                          'SYSTEMD CRITICAL - test.service: failed\n')
+
+    def test_failure_verbose(self):
+        with AddBin('bin/failure'):
+            process = subprocess.run(
+                ['./check_systemd.py', '-v'],
+                encoding='utf-8',
+                stdout=subprocess.PIPE,
+            )
+        self.assertEqual(process.returncode, 2)
+        self.assertEqual(
+            process.stdout,
+            'SYSTEMD CRITICAL - test.service: failed\n'
+            'critical: test.service: failed\n'
+            'ok: all\n'
+        )
+
+    def test_failure_multiple(self):
+        with AddBin('bin/multiple_failure'):
+            process = subprocess.run(
+                ['./check_systemd.py'],
+                encoding='utf-8',
+                stdout=subprocess.PIPE,
+            )
+        self.assertEqual(process.returncode, 2)
+        self.assertEqual(
+            process.stdout,
+            'SYSTEMD CRITICAL - test1.service: failed, '
+            'test2.service: failed\n'
+        )
+
+    def test_failure_multiple_verbose(self):
+        with AddBin('bin/multiple_failure'):
+            process = subprocess.run(
+                ['./check_systemd.py', '--verbose'],
+                encoding='utf-8',
+                stdout=subprocess.PIPE,
+            )
+        self.assertEqual(process.returncode, 2)
+        self.assertEqual(
+            process.stdout,
+            'SYSTEMD CRITICAL - test1.service: failed, '
+            'test2.service: failed\n'
+            'critical: test1.service: failed\n'
+            'critical: test2.service: failed\n'
+            'ok: all\n'
+        )
 
     def test_exclusive_group(self):
         process = subprocess.run(
