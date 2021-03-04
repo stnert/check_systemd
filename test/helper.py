7959ce9e8c96533e0956f70b8915fbf3f50ed88a
Josef Friedrich <josef@friedrich.rocks>
2021-03-04 12:22:34 +0100

    Test help

---
+++ test/helper.py
@@ -0,0 +1,51 @@
+from io import StringIO
+import sys
+import re
+
+# https://github.com/Josef-Friedrich/jflib/blob/master/jflib/capturing.py
+
+
+class Capturing(list):
+    """Capture the stdout or stderr output. This class is designed for unit
+    tests.
+    :param stream: `stdout` or `stderr`.
+    :param clean_ansi: Clean out ANSI colors from the captured output.
+    .. seealso::
+        `Answer on Stackoverflow <https://stackoverflow.com/a/16571630>`_
+    """
+
+    def __init__(self, stream: str = 'stdout', clean_ansi: bool = False):
+        if stream not in ['stdout', 'stderr']:
+            raise(ValueError('“stream” must be either “stdout” or “stderr”'))
+        self.stream = stream
+        self.clean_ansi = clean_ansi
+
+    def __enter__(self):
+        if self.stream == 'stdout':
+            self._pipe = sys.stdout
+            sys.stdout = self._stringio = StringIO()
+        elif self.stream == 'stderr':
+            self._pipe = sys.stderr
+            sys.stderr = self._stringio = StringIO()
+        return self
+
+    def __exit__(self, *args):
+        if self.clean_ansi:
+            output = self._clean_ansi(self._stringio.getvalue())
+        else:
+            output = self._stringio.getvalue()
+        self.extend(output.splitlines())
+        del self._stringio
+        if self.stream == 'stdout':
+            sys.stdout = self._pipe
+        elif self.stream == 'stderr':
+            sys.stderr = self._pipe
+
+    def tostring(self):
+        """Convert the output into an string. By default a list of output
+        lines is returned."""
+        return '\n'.join(self)
+
+    @staticmethod
+    def _clean_ansi(text):
+        return re.sub(r'\x1b.*?m', '', text)
--- test/test_mock.py
+++ test/test_mock.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest import mock
 import check_systemd
+from helper import Capturing
 
 
 def mock_main(argv=['check_systemd.py'], stdout=None, stderr=None):
@@ -21,6 +22,16 @@ class TestMock(unittest.TestCase):
     def test_ok(self):
         result = mock_main()
         result['sys_exit'].assert_called_with(0)
+
+    def test_help(self):
+        with mock.patch('sys.exit'), \
+                mock.patch('sys.argv', ['check_systemd.py', '--help']):
+
+            with Capturing() as output:
+                check_systemd.main()
+
+        self.assertIn('usage: check_systemd', output.tostring())
+        # sys_exit.assert_called_with(0)
 
 
 if __name__ == '__main__':
