4352a6dcf1b4e0cc840da13a286840b9cdef1acd
Josef Friedrich <josef@friedrich.rocks>
2022-01-28 23:39:48 +0100

    Investigate dbus api

--- test/dbus_systemd_info.py
+++ test/dbus_systemd_info.py
@@ -25,6 +25,11 @@ dbus = DBusProxy.new_for_bus_sync(BusType.SYSTEM, 0, None,
 
 
 def load_unit(unit_name, interface='Unit'):
+    """
+    :param: The name of the systemd unit. For example apt-daily.timer or
+      nginx.service
+    :param interface: For example Unit or Timer
+    """
     try:
         loaded_unit = dbus.LoadUnit('(s)', unit_name)
     except Exception as e:
@@ -45,13 +50,20 @@ def get_unit_property(unit, property_name):
     return unit.get_cached_property(property_name).unpack()
 
 
-print(dbus.get_name())
+
+def list_properties_of_unit(unit_name):
+    unit = load_unit(unit_name)
+    for property_name in unit.get_cached_property_names():
+        print(property_name)
+
+
+# print(dbus.get_name())
 # org.freedesktop.systemd1
 
-print(dbus.get_flags())
+# print(dbus.get_flags())
 # <flags 0 of type Gio.DBusProxyFlags>
 
-print(dbus.get_cached_property_names())
+# print(dbus.get_cached_property_names())
 # ['Architecture', 'ConfirmSpawn', 'ControlGroup', 'DefaultBlockIOAccounting',
 # 'DefaultCPUAccounting', 'DefaultLimitAS', 'DefaultLimitASSoft',
 # 'DefaultLimitCORE', 'DefaultLimitCORESoft', 'DefaultLimitCPU',
@@ -93,12 +105,12 @@ print(dbus.get_cached_property_names())
 # 'UserspaceTimestamp', 'UserspaceTimestampMonotonic', 'Version',
 # 'Virtualization']
 
-print(dbus.get_cached_property('Version'))
+# print(dbus.get_cached_property('Version'))
 # '245.4-4ubuntu3.2'
 
 # print(dbus.ListUnits())
 
-# https://www.freedesktop.org/wiki/Software/systemd/dbus/
+# https://www.freedesktop.org/software/systemd/man/org.freedesktop.systemd1.html
 # ListUnits() returns an array with all currently loaded units. Note that units
 # may be known by multiple names at the same name, and hence there might be
 # more unit names loaded than actual units behind them. The array consists of
@@ -117,56 +129,63 @@ def print_unit_property(dbus_unit, property_name):
                           get_unit_property(dbus_unit, property_name)))
 
 
-# ('time-set.target', 'System Time Set', 'loaded', 'active', 'active', '',
-# '/org/freedesktop/systemd1/unit/time_2dset_2etarget', 0, '', '/')
-for (name, description, load, active, sub_state, followed_unit, object_path,
-     job_id, job_type, job_object_path) in dbus.ListUnits():
-    print('\n{}\n'.format(color(name, fg='red')))
+def list_all_units():
+    # ('time-set.target', 'System Time Set', 'loaded', 'active', 'active', '',
+    # '/org/freedesktop/systemd1/unit/time_2dset_2etarget', 0, '', '/')
+    for (name, description, load, active, sub_state, followed_unit, object_path,
+        job_id, job_type, job_object_path) in dbus.ListUnits():
+        print('\n{}\n'.format(color(name, fg='red')))
 
-    # The primary unit name as string
-    print_unit_structure("Primary unit name", name)
+        # The primary unit name as string
+        print_unit_structure("Primary unit name", name)
 
-    # The human readable description string
-    print_unit_structure("Human readable description", description)
+        # The human readable description string
+        print_unit_structure("Human readable description", description)
 
-    # The load state (i.e. whether the unit file has been loaded successfully)
-    print_unit_structure("The load state", load)
+        # The load state (i.e. whether the unit file has been loaded successfully)
+        print_unit_structure("The load state", load)
 
-    # The active state (i.e. whether the unit is currently started or not)
-    print_unit_structure("The active state", active)
+        # The active state (i.e. whether the unit is currently started or not)
+        print_unit_structure("The active state", active)
 
-    # The sub state (a more fine-grained version of the active state that is
-    # specific to the unit type, which the active state is not)
-    print_unit_structure("The sub state", sub_state)
+        # The sub state (a more fine-grained version of the active state that is
+        # specific to the unit type, which the active state is not)
+        print_unit_structure("The sub state", sub_state)
 
-    # A unit that is being followed in its state by this unit, if there is any,
-    # otherwise the empty string.
-    print_unit_structure("The followed unit", followed_unit)
+        # A unit that is being followed in its state by this unit, if there is any,
+        # otherwise the empty string.
+        print_unit_structure("The followed unit", followed_unit)
 
-    print_unit_structure("The unit object path", object_path)
+        print_unit_structure("The unit object path", object_path)
 
-    # If there is a job queued for the job unit the numeric job id, 0 otherwise
-    print_unit_structure("The numeric job id", job_id)
+        # If there is a job queued for the job unit the numeric job id, 0 otherwise
+        print_unit_structure("The numeric job id", job_id)
 
-    # The job type as string
-    print_unit_structure("The job type", job_type)
+        # The job type as string
+        print_unit_structure("The job type", job_type)
 
-    print_unit_structure("The job object path", job_object_path)
+        print_unit_structure("The job object path", job_object_path)
 
-    dbus_unit = load_unit(name)
+        dbus_unit = load_unit(name)
 
-    print_unit_property(dbus_unit, 'ActiveState')
+        print_unit_property(dbus_unit, 'ActiveState')
 
-# timers
-for (name, description, load, active, sub_state, followed_unit, object_path,
-     job_id, job_type, job_object_path) in dbus.ListUnits():
 
-    if '.timer' in name:
-        print(name)
-        dbus_unit = load_unit(name, 'Timer')
-        try:
-            print_unit_property(dbus_unit, 'TimersMonotonic')
-            print_unit_property(dbus_unit, 'NextElapseUSecRealtime')
+def list_all_timers():
+    # timers
+    for (name, description, load, active, sub_state, followed_unit, object_path,
+        job_id, job_type, job_object_path) in dbus.ListUnits():
 
-        except Exception as e:
-            pass
+        if '.timer' in name:
+            print(name)
+            dbus_unit = load_unit(name, 'Timer')
+
+            try:
+                print_unit_property(dbus_unit, 'TimersMonotonic')
+                print_unit_property(dbus_unit, 'NextElapseUSecRealtime')
+
+            except Exception as e:
+                pass
+
+
+list_properties_of_unit('apt-daily.timer')
