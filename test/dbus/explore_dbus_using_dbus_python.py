57dec4e13f18974444ec758020e5f19fb993c257
Josef Friedrich <josef@friedrich.rocks>
2022-01-30 11:05:25 +0100

    Improve dbus test scripts

--- test/dbus/explore_dbus_using_dbus_python.py
+++ test/dbus/explore_dbus_using_dbus_python.py
@@ -4,6 +4,8 @@
 A little script to explore the D-Bus API of systemd using dbus-python.
 """
 
+import utils
+
 # https://dbus.freedesktop.org/doc/dbus-python/
 
 import dbus
@@ -21,23 +23,36 @@ manager = dbus.Interface(
     'org.freedesktop.systemd1.Manager'
 )
 
-unit_state = manager.GetUnitFileState(unit_name)
-print(unit_state)
+
+def print_all_properties_of_object(object_path: str, interface_name='org.freedesktop.systemd1.Unit') -> str:
+    """
+    :param object_path: for example /org/freedesktop/systemd1/unit/apt_2ddaily_2eservice
+    :param interface_name: for example org.freedesktop.systemd1.Service
+    """
+
+    print(utils.format_colorized_heading(interface_name))
+    proxy = bus.get_object('org.freedesktop.systemd1',
+                           object_path,)
+
+    interface = dbus.Interface(
+        proxy, dbus_interface="org.freedesktop.DBus.Properties")
+
+    # https://dbus.freedesktop.org/doc/dbus-specification.html#standard-interfaces-properties
+    properties = interface.GetAll(
+        interface_name)
+    for key, value in properties.items():
+        print(utils.colorize_key_value(key, value))
 
 
-unit = manager.LoadUnit(unit_name)
-print(unit)
+for (name, description, load, active, sub_state, followed_unit, object_path,
+        job_id, job_type, job_object_path) in manager.ListUnits():
+    print()
+    print(name)
+    print(object_path)
 
+    proxy = bus.get_object('org.freedesktop.systemd1',
+                           object_path,)
 
-proxy = bus.get_object('org.freedesktop.systemd1',
-                      '/org/freedesktop/systemd1/unit/apt_2ddaily_2etimer',)
-
-interface = dbus.Interface(
-    proxy, dbus_interface="org.freedesktop.DBus.Properties")
-# name = interface.Get("org.freedesktop.systemd1.Unit", 'Id')
-
-
-properties = interface.GetAll(
-    "org.freedesktop.systemd1.Unit")
-for key, value in properties.items():
-    print('{}: {}'.format(key, value))
+    print_all_properties_of_object(object_path)
+    print_all_properties_of_object(
+        object_path, utils.get_interface_name_from_object_path(object_path))
---
+++ test/dbus/utils.py
@@ -0,0 +1,36 @@
+from colors import color
+
+
+def colorize_key_value(key: any, value: any) -> str:
+    key = str(key)
+    value = str(value)
+
+    """Print a value of the structure returned by ListUnits()"""
+    return "{}: {}".format(color(key, fg='green'),
+                           color(value, fg='yellow'))
+
+
+def format_colorized_heading(heading: str) -> str:
+    return '\n{}'.format(color(heading, fg='red'))
+
+
+def get_interface_name_from_unit_name(unit_name: str) -> str:
+    """
+    :param name: for example apt-daily.service
+
+    :return: org.freedesktop.systemd1.Service
+    """
+    name_segments = unit_name.split('.')
+    interface_name = name_segments[-1]
+    return 'org.freedesktop.systemd1.{}'.format(interface_name.title())
+
+
+def get_interface_name_from_object_path(object_path: str) -> str:
+    """
+    :param object_path: for example /org/freedesktop/systemd1/unit/apt_2ddaily_2eservice
+
+    :return: org.freedesktop.systemd1.Service
+    """
+    name_segments = object_path.split('_2e')
+    interface_name = name_segments[-1]
+    return 'org.freedesktop.systemd1.{}'.format(interface_name.title())
