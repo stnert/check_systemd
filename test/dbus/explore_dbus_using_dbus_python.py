979c8054b7eae0c745a0f097496624f1363deb16
Josef Friedrich <josef@friedrich.rocks>
2022-01-30 09:05:37 +0100

    Investigate package dbus-python

--- test/dbus/explore_dbus_using_dbus_python.py
+++ test/dbus/explore_dbus_using_dbus_python.py
@@ -1,14 +1,14 @@
 #! /usr/bin/python3
 
 """
-A little script to explore the D-Bus API of systemd.
+A little script to explore the D-Bus API of systemd using dbus-python.
 """
 
 # https://dbus.freedesktop.org/doc/dbus-python/
 
 import dbus
 
-unit_name = 'sshd.service'
+unit_name = 'apt-daily.timer'
 
 bus = dbus.SystemBus()
 systemd = bus.get_object(
@@ -27,3 +27,17 @@ print(unit_state)
 
 unit = manager.LoadUnit(unit_name)
 print(unit)
+
+
+proxy = bus.get_object('org.freedesktop.systemd1',
+                      '/org/freedesktop/systemd1/unit/apt_2ddaily_2etimer',)
+
+interface = dbus.Interface(
+    proxy, dbus_interface="org.freedesktop.DBus.Properties")
+# name = interface.Get("org.freedesktop.systemd1.Unit", 'Id')
+
+
+properties = interface.GetAll(
+    "org.freedesktop.systemd1.Unit")
+for key, value in properties.items():
+    print('{}: {}'.format(key, value))
